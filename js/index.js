625956cc53cf53a246cd9aea7f8e57f7af28ab61
mariemabdelsalam <mariemabdelsalam61@gmail.com>
2025-09-30 13:55:16 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let navbar = this.document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
